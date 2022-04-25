12c9ef172e5e5c2552081e5ffd34866090e10a97
Xeeshan123 <zeeshan.awan557@gmail.com>
2022-04-25 22:13:05 +0500

    New Files
    Updated code is now available

---
+++ rssfeed.php
@@ -0,0 +1,45 @@
+<?php
+
+cnews_header();
+
+?>
+<!--====== HEADER PART ENDS ======-->
+<!--====== MAIN PART ENDS ======-->
+<main>
+    <!--====== NEWS PART START ======-->
+    <section id="heading_news">
+        <center>
+            <div class="thecuratedflow_img_area">
+                <img src="./static/images/topnews.png" alt="top" class="img-fluid">
+            </div>
+        </center>
+        <div class="container">
+            <div class="wrap_news">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="news_head">
+                            <h1 class="heading_title">A Headlines Oriented Approach</h1>
+                            <p class="heaading_shortInfo">Curated, Maintained, and Crowd-Sourced. Factual information through randomization. &copy;2020</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+
+                        <script src="//rss.bloople.net/?url=https%3A%2F%2Freason.com%2Flatest%2Ffeed%2F&showtitle=false&type=js"></script>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--====== NEWS PART ENDS ======-->
+</main>
+<!--====== MAIN PART ENDS ======-->
+<!--======  footer area ======-->
+
+<?php
+
+cnews_footer();
+
+?>
